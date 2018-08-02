8817b14b5bf33c6cb5d85ab0151565bbd4d85289
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2018-08-03 02:10:41 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using SampleSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string connection { get; set; }
+        // GET: Home
+        [SessionAuthorize]
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetTaskName()
+        {
+            try
+            {
+                connection = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+                string jResult = string.Empty;
+                using (SqlConnection con = new SqlConnection(connection))
+                {
+                    using (SqlCommand cmd = new SqlCommand("USPDropDownBind"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        cmd.Parameters.AddWithValue("@Action", 1);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader dataReader = cmd.ExecuteReader();
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(dataReader);
+                        con.Close();
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            jResult = JsonConvert.SerializeObject(dataTable, Newtonsoft.Json.Formatting.Indented);
+                            if (jResult != string.Empty)
+                                return Json(jResult, JsonRequestBehavior.AllowGet);
+                        }
+                        else
+                        {
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                Dispose();
+            }
+            return Json(new { flag = false }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/Common.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SampleSite.Models
+{
+    public class Common
+    {
+    }
+    public class SessionAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return (httpContext.Session["employeeID"] != null);
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                                  new RouteValueDictionary
+                                  {
+                                   { "action", "Login" },
+                                   { "controller", "Login" }
+                                  });
+        }
+    }
+}
